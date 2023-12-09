2619b77148628babda60d66c517737186d69a449
Sean <moriorpandemic@gmail.com>
2023-12-09 16:32:27 +0600

    added posibility to modify logger params

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"time"
@@ -11,7 +12,8 @@ import (
 type Config struct {
 	Env        string `yaml:"env" env-required:"true"`
 	Storage    string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"http_server"`
+	Logger     `yaml:"logger" env-required:"true"`
+	HTTPServer `yaml:"http_server" env-required:"true"`
 }
 
 type HTTPServer struct {
@@ -20,23 +22,27 @@ type HTTPServer struct {
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-required:"true"`
 }
 
-func LoadCFG() (string, *Config) {
+type Logger struct {
+	LogType  string `yaml:"log_type" env-required:"true"`
+	LogLevel int8   `yaml:"log_level" env-required:"true"`
+}
+
+func LoadCFG() (error, *Config) {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		return fmt.Sprint("CONFIG_PATH is not defined"), nil
+		return errors.New("CONFIG_PATH is not defined"), nil
 	}
 
 	// check if file exist
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		return fmt.Sprintf("config file '%s' does not exist", configPath), nil
+		return errors.New(fmt.Sprintf("config file '%s' does not exist", configPath)), nil
 	}
 
 	var cfg Config
 
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		return fmt.Sprintf("cannot read config: %s", err), nil
+		return errors.New(fmt.Sprintf("cannot read config: %s", err)), nil
 	}
 
-	return "", &cfg
-
+	return nil, &cfg
 }
