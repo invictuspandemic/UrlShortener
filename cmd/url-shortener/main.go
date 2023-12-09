0467a5ef943876f7bd6b3cd0dff20f053005b3cd
Sean <moriorpandemic@gmail.com>
2023-12-09 16:37:16 +0600

    util for slog, to log errors more readable way

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,25 +3,73 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/cslog"
+	"url-shortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
-	error_msg, cfg := config.LoadCFG()
-	if cfg == nil {
-		log.Fatal(error_msg)
+	err, cfg := config.LoadCFG()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// _rnl_
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Logger)
 
-	// TODO: init logger: slog
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug message are enabled")
 
-	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.Storage)
+
+	if err != nil {
+		log.Error(fmt.Sprintf("failed to init storage: '%s'", cfg.Storage), cslog.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
 
+}
+
+func setupLogger(loggerCFG config.Logger) *slog.Logger {
+	var log *slog.Logger
+	switch loggerCFG.LogType {
+	case "text":
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{
+					Level: slog.Level(loggerCFG.LogLevel),
+				},
+			),
+		)
+	case "json":
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout, &slog.HandlerOptions{
+					Level: slog.Level(loggerCFG.LogLevel),
+				},
+			),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout, &slog.HandlerOptions{
+					Level: slog.LevelInfo,
+				},
+			),
+		)
+	}
+	return log
 }
